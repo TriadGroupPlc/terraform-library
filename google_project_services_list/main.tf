2718c5cf49254d5354f548df0b641bb31f859812
rhodium289 <henry.grechcini@gmail.com>
2022-06-09 15:59:50 +0100

    Keep the original folder structure

--- google_project_services_list/main.tf
+++
@@ -1,7 +0,0 @@
-# loop to enable the required services
-resource "google_project_service" "project" {
-  for_each = toset(var.services)
-  project = "${var.project_id}"
-  service = each.value
-  disable_dependent_services = true
-}
